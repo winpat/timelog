bdc0d517d0d8e1072f36c5dcd26314e6024085cc
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2019-05-11 17:46:28 +0200

    Add simple clock-in and clock-out functionality (WIP)

--- src/Cli.hs
+++ src/Cli.hs
@@ -2,12 +2,12 @@ module Cli where
 
 import Types
 
+import Data.List (sort)
 import Control.Monad (when)
 import qualified UI as UI(main)
 import System.Exit (die)
 
 import Data.Time.Clock
-
 
 commandLine :: [String] -> Log -> IO Log
 commandLine args l = do
@@ -18,4 +18,32 @@ commandLine args l = do
 handleArgument :: String -> Log -> IO Log
 handleArgument a l
              | a == "ui"    = UI.main l
+             | a == "start" = clockIn l
+             | a == "stop"  = clockOut l
              | otherwise    = undefined
+
+clockIn :: Log -> IO Log
+clockIn l = do
+  time <- getCurrentTime
+  putStrLn ("Clocked in at " ++ show time)
+  return $ Entry { startTime = Just time
+                 , endTime = Nothing
+                 , description = "Work work work" } : l
+
+clockOut :: Log -> IO Log
+clockOut l = do
+  time <- getCurrentTime
+  putStrLn ("Clocked out at " ++ show time)
+  let cur = mostRecentEntry l
+      new = (Entry { startTime = (startTime cur)
+                   , endTime = Just time
+                   , description = (description cur) })
+  return $ replaceEntry l cur new
+
+replaceEntry :: Log -> Entry -> Entry -> Log
+replaceEntry (l:ls) o n
+    | l == o    = n : replaceEntry ls o n
+    | otherwise = l : replaceEntry ls o n
+
+mostRecentEntry :: Log -> Entry
+mostRecentEntry l = head $ sort l
--- src/Types.hs
+++ src/Types.hs
@@ -7,9 +7,15 @@ import Data.Time.Clock.POSIX
 data Entry = Entry {
     startTime :: Maybe UTCTime
   , endTime :: Maybe UTCTime
-  , description :: String 
+  , description :: String
 }  deriving (Eq, Read, Show)
 
+-- TODO: Does deriving Eq do the same?
+-- instance Eq Entry where
+--   (Entry s1 e1 d1) == (Entry s2 e2 d2) = s1 == s2 && e1 == e2 && d1 == d2
+
+instance Ord Entry where
+  (Entry s1 _ _) `compare` (Entry s2 _ _) = s1 `compare` s2
 
 type Log = [Entry]
 
@@ -23,6 +29,6 @@ renderDate (Just date) =
 renderDateDiff :: Maybe UTCTime -> Maybe UTCTime -> String
 renderDateDiff Nothing _ = ""
 renderDateDiff _ Nothing = ""
-renderDateDiff (Just start) (Just end) = 
+renderDateDiff (Just start) (Just end) =
         let format = "%H:%M"
         in formatTime defaultTimeLocale format . posixSecondsToUTCTime $ diffUTCTime end start
